39fb7e22eff68f10f78a967f95de13dc8a99213d
Eduardo Oliveira <eduardo.oliveira@estudante.ufjf.br>
2022-03-15 22:37:03 -0300

    cria 3 tamanhos diferentes e aleatorios para o mosquito

--- code.js
+++ code.js
@@ -21,7 +21,7 @@ function posicaoRandomica(){
 
     var mosquito = document.createElement('img');
     mosquito.src = 'img/mosquito.png';
-    mosquito.className = 'mosquito1';
+    mosquito.className = tamanhoAleatorio();
     mosquito.style.left = posicaoX + 'px';
     mosquito.style.top = posicaoY + 'px';
     mosquito.style.position = 'absolute';
@@ -29,3 +29,15 @@ function posicaoRandomica(){
     document.body.appendChild(mosquito);
 }
 
+function tamanhoAleatorio(){
+    var classe = Math.floor(Math.random() * 3);
+
+    switch(classe){
+        case 0:
+            return 'mosquito1';
+        case 1:
+            return 'mosquito2';
+        case 2:
+            return 'mosquito3';
+    }
+}
